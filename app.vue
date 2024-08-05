e94e6c7544e4d88963d0d8dd338109986d21a8f0
Sambert Rodrigues <sambert@smartshore.nl>
2024-08-05 14:06:42 +0530

    first commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <header
+      class="header sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+      <a href="">cartrader</a>
+    </header>
+  </div>
+</template>
