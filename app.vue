a06ca7c19088f52324fff2b5759df85bfb3783d9
Sambert Rodrigues <sambert@smartshore.nl>
2024-08-06 18:38:28 +0530

    DynamicRouter
    Programatic Navigation
    useHead

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <NuxtPage />
+    <Client-Only>
+      <NuxtPage />
+    </Client-Only>
   </div>
 </template>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
   <header class="header sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
-    <a class="text-3xl font0mono" href="/">cartrader</a>
+    <NuxtLink class="text-3xl font0mono" to="/">cartrader</NuxtLink>
   </header>
 </template>
