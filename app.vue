97d30a88d3975a2de1c8e8029d14274962124cdf
Sambert Rodrigues <sambert@smartshore.nl>
2024-08-06 13:01:49 +0530

    building ui elements

--- app.vue
+++ app.vue
@@ -1,23 +1,11 @@
 <template>
   <div>
-    <!-- NAVIGATION -->
     <header
       class="header sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
       <a class="text=3xl font0mono" href="/">cartrader</a>
     </header>
-    <!-- NAVIGATION -->
-    <!-- HOME HERO -->
-    <div
-      class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://w.wallhaven.cc/full/x6/wallhaven-x6gy6d.jpg')]">
-      <div class="z-10 absolute w-full h-full flex justify-center items-center">
-        <div class="text-center w-1/2">
-          <h1 class="text-8xl font-serif mb-1 text-white">Find your drive</h1>
-        </div>
-      </div>
-      <div class="z-1 absolute w-full h-full bg-black opacity-50">
-      </div>
-    </div>
-    <!-- bg-black/70  -->
-    <!-- HOME HERO -->
+
+
+
   </div>
 </template>
