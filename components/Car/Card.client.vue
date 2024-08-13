0ca6b41c52e7bf18fea3c75e71cf5170147fed35
Sambert Rodrigues <sambert@smartshore.nl>
2024-08-13 15:43:38 +0530

    Vueuse
    fixed port
    adde animation to card
    added loclhost storage

---
+++ components/Car/Card.client.vue
@@ -0,0 +1,32 @@
+<script setup>
+import heartFilled from '@/assets/icons/heartFilled.png'
+import heartOutline from '@/assets/icons/heartOutline.png'
+const props = defineProps({
+  car: Object,
+  favourite: Boolean
+})
+
+//removed and moved to parent
+// const favourite = useState(`favourite-${props.car?.id}`, () => {
+//   return false
+// })
+
+const emit = defineEmits(['favor'])
+</script>
+<template>
+  <div class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px] group">
+    <img :src="favourite ? heartFilled : heartOutline" alt="camaro" class="absolute w-7 right-5 top-2 z-20"
+      @click="emit('favor', car.id)">
+    <div class="flex h-full" @click="navigateTo(`/car/${car?.name}-${car?.id}`)">
+      <div class="thumbnail flex overflow-hidden">
+        <NuxtImg :src="car?.image" alt="camaro"
+          class="w-[300px] h-full object-cover transition-transform duration-300 ease-in-out transform group-hover:scale-150 z-10" />
+      </div>
+      <div class="w-full p-4 flex flex-col">
+        <h1 class="text-xl text-red-800">{{ car?.name }}</h1>
+        <p class="text-gray-700">{{ car?.description }}</p>
+        <h1 class="mt-auto text-2xl">${{ car?.price }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
--- components/Car/Cards.vue
+++ components/Car/Cards.vue
@@ -1,8 +1,20 @@
 <script setup>
 const { cars } = useCars()
+
+const favourite = useLocalStorage('favourite', {})
+const handleFavourite = (carId) => {
+  if (carId in favourite.value) {
+    delete favourite.value[carId]
+  } else {
+    favourite.value = {
+      ...favourite.value,
+      [carId]: true
+    }
+  }
+}
 </script>
 <template>
   <div class="w-full">
-    <CarCard v-for="car in cars" :key="car.id" :car="car" />
+    <CarCard v-for="car in cars" :key="car.id" :car="car" @favor="handleFavourite" :favourite="car.id in favourite" />
   </div>
 </template>
