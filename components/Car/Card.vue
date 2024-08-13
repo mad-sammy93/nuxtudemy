8f02122a27190e8159b92e1fce6b9baf588d6709
Sambert Rodrigues <sambert@smartshore.nl>
2024-08-13 11:15:30 +0530

    Added favouirte icons

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,4 +1,6 @@
 <script setup>
+import heartFilled from '@/assets/icons/heartFilled.png'
+import heartOutline from '@/assets/icons/heartOutline.png'
 const props = defineProps({
   car: Object
 })
@@ -6,9 +8,9 @@ const props = defineProps({
 <template>
   <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
     @click="navigateTo(`/car/${car?.name}-${car?.id}`)">
+    <img src="" alt="camaro" class="absolute w-7 right-5 top-2 z-20">
     <div class="flex h-full">
-      <img :src="car?.image" alt="camaro" class="w-[300px] h-full 
-      object-cover">
+      <NuxtImg :src="car?.image" alt="camaro" class="w-[300px] h-full object-cover" />
       <div class="p-4 flex flex-col">
         <h1 class="text-xl text-red-800">{{ car?.name }}</h1>
         <p class="text-gray-700">{{ car?.description }}</p>
--- components/Car/SearchBar.vue
+++ components/Car/SearchBar.vue
@@ -13,7 +13,7 @@ const handleSearch = () => {
   <div
     class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-betweeen overflow-hidden drop-shadow-2xl mx-auto">
     <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="Search by city"
-      :class="cityError ? 'border-red-500 border' : ''" v-model="city" />
+      :class="cityError ? 'border-red-500 border' : ''" v-model="city" @keyup.enter="handleSearch" />
     <button class="bg-cyan-950 px-10 text-white" @click="handleSearch">Search</button>
   </div>
 </template>
