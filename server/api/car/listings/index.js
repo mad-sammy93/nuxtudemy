621e9a9ed24f411dcaa7897927656a11d7e55d6a
Sambert Rodrigues <sambert@smartshore.nl>
2024-08-21 12:19:08 +0530

    changes to schema

--- server/api/car/listings/index.js
+++ server/api/car/listings/index.js
@@ -7,6 +7,7 @@ const schema = Joi.object({
   miles: Joi.number().required().min(0),
   city: Joi.string().min(2).required(),
   numberOfseats: Joi.number().max(1000).min(1).required(),
+  description: Joi.string().min(20).required(),
   featiures: Joi.array().items(Joi.string()).required(),
   image: Joi.string().uri().required(),
   listerId: Joi.string().required(),
@@ -19,5 +20,24 @@ export default defineEventHandler(async (event) => {
   if (error) {
     throw createError({ statusCode: error.statusCode, statusMessage: error.message })
   }
+
+  const { image, name, numberOfseats, listerId, price, city, featiures, miles, make, model, year, description } = body
+
+  const car = await prisma.car.create({
+    data: {
+      image,
+      name,
+      numberOfseats,
+      listerId,
+      price,
+      city: city.toLowerCase(),
+      featiures,
+      miles,
+      make,
+      model,
+      year,
+      description,
+    },
+  })
   return { error, value }
 })
